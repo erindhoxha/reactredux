d8c2dd3a229b16be4d4831990d1aa36a081555f2
Erind Hoxha <erind_cbh@hotmail.com>
2020-03-25 13:51:02 +1300

    tutorial 154

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,27 +4,27 @@ import { selectSong } from "../actions";
 
 class SongList extends Component {
     constructor(props) {
-        console.log(props);
         super(props);
         this.state = {
         }
-    }
-    click = (song) => {
-       this.props.dispatch(selectSong(song));
-       console.log(this.props);
     }
     render() {
         return (
             <div className="ui list">
                     {this.props.songs.map(song => {
                         return (
-                        <div className="item" key={song.title} onClick={() => this.click(song)}>
+                          <div className="item" key={song.title}>
                             <div className="right floated content">
-                                <button className="ui button primary">Select</button>
+                              <button
+                                className="ui button primary"
+                                onClick={() => this.props.selectSong(song)}
+                              >
+                                Select
+                              </button>
                             </div>
                             <div className="content">{song.title}</div>
                             <div className="content">{song.duration}</div>
-                        </div>
+                          </div>
                         );
                     })}
             </div>
@@ -32,11 +32,12 @@ class SongList extends Component {
     }
 }
 
-const getMyState = state => {
+const mapStateToProps = state => {
+    console.log(state);
     return { 
         songs: state.songs,
         selectedSong: state.selectedSong
     };
 }
 
-export default connect(getMyState)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,27 @@
 import { combineReducers } from "redux";
 
-const songsReducer = () => {
+const songsReducer = (songs, action) => {
+    if (action) {
     return [
-        {
-            title: 'First song',
-            duration: '4:05',
-        },
-        {
-            title: 'Second song',
-            duration: '3:45',
-        },
-        {
-            title: 'Third song',
-            duration: '2:15',
-        }
+      {
+        title: "First song",
+        duration: "4:05"
+      },
+      {
+        title: "Second song",
+        duration: "3:45"
+      },
+      {
+        title: "Third song",
+        duration: "2:15"
+      }
     ];
+    }
+
 }
 
 const selectedSongReducer = (selectedSong = null, action) => {
-    if (action.type == 'SONG_SELECTED') {
+    if (action.type === 'SONG_SELECTED') {
         return action.payload.song;
     }
     return selectedSong;
